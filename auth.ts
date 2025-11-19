1c6f911a866781cfdb5a8f3ac5f2b35959bb3f5d
Tiger <watthanon.tb@gmail.com>
2025-11-20 01:53:06 +0700

    Create auth.ts

---
+++ auth.ts
@@ -0,0 +1,13 @@
+// auth.ts
+import NextAuth from "next-auth";
+import Google from "next-auth/providers/google";
+
+export const { auth, handlers, signIn, signOut } = NextAuth({
+  providers: [
+    Google({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
+  ],
+  secret: process.env.NEXTAUTH_SECRET,
+});
